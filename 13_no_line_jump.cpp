5aff0579c9a05df40eae7ce2623d565a280a03a9
Kumar Ayush <109751187+ayushssshhh@users.noreply.github.com>
2022-11-13 20:23:25 +0530

    Create 13_no_line_jump.cpp

---
+++ 13_no_line_jump.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    string s;
+    
+    if((v1>v2) && (x1>x2))
+    s = "NO";
+    
+    else if((v1<v2) && (x1<x2))
+    s = "NO";
+    
+    else if(x1<x2){
+        for(int i=0 ; i<pow(2,64) ; i++){
+            x1 += v1;
+            x2 += v2;
+            if(x1 == x2){
+                s = "YES";
+                break;
+            }
+            
+            if(x1>x2){
+                s = "NO";
+                break;
+            }
+        }
+    }
+    
+    else{
+        for(int i=0 ; i<pow(2,64) ; i++){
+            x1 += v1;
+            x2 += v2;
+            if(x1 == x2){
+                s = "YES";
+                break;
+            }
+            
+            if(x1<x2){
+                s = "NO";
+                break;
+            }
+        }
+    } 
+
+    return s;
+}
+
+int main()
+{
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int x1 = stoi(first_multiple_input[0]);
+
+    int v1 = stoi(first_multiple_input[1]);
+
+    int x2 = stoi(first_multiple_input[2]);
+
+    int v2 = stoi(first_multiple_input[3]);
+
+    string result = kangaroo(x1, v1, x2, v2);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
